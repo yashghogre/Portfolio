1dda1d74a79198bfdc0a5ce5fa380966f876a95a
Yash <yashghogre100@gmail.com>
2023-10-15 15:36:24 +0530

    favicon and title attempt

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -22,6 +22,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <Head>
+        <title>Yash Ghogre</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet" />
